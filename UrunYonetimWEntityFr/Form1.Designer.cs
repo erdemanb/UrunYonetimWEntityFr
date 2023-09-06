03f29717de07655b862c2815586701c0a9377fca
H. Erdem Anbarkaya <anbarkaya46@gmail.com>
2023-09-06 13:57:48 +0300

    Add project files.

---
+++ UrunYonetimWEntityFr/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace UrunYonetimWEntityFr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKategoriID = new System.Windows.Forms.Label();
+            this.txtKategoriID = new System.Windows.Forms.TextBox();
+            this.txtKategoriAd = new System.Windows.Forms.TextBox();
+            this.lblKategoriAd = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKategoriID
+            // 
+            this.lblKategoriID.AutoSize = true;
+            this.lblKategoriID.Location = new System.Drawing.Point(14, 25);
+            this.lblKategoriID.Name = "lblKategoriID";
+            this.lblKategoriID.Size = new System.Drawing.Size(71, 15);
+            this.lblKategoriID.TabIndex = 0;
+            this.lblKategoriID.Text = "Kategori ID:";
+            // 
+            // txtKategoriID
+            // 
+            this.txtKategoriID.Location = new System.Drawing.Point(91, 22);
+            this.txtKategoriID.Name = "txtKategoriID";
+            this.txtKategoriID.Size = new System.Drawing.Size(100, 21);
+            this.txtKategoriID.TabIndex = 1;
+            // 
+            // txtKategoriAd
+            // 
+            this.txtKategoriAd.Location = new System.Drawing.Point(91, 49);
+            this.txtKategoriAd.Name = "txtKategoriAd";
+            this.txtKategoriAd.Size = new System.Drawing.Size(100, 21);
+            this.txtKategoriAd.TabIndex = 3;
+            // 
+            // lblKategoriAd
+            // 
+            this.lblKategoriAd.AutoSize = true;
+            this.lblKategoriAd.Location = new System.Drawing.Point(12, 52);
+            this.lblKategoriAd.Name = "lblKategoriAd";
+            this.lblKategoriAd.Size = new System.Drawing.Size(73, 15);
+            this.lblKategoriAd.TabIndex = 2;
+            this.lblKategoriAd.Text = "Kategori Ad:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(230, 34);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(392, 34);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(311, 34);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(480, 164);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 270);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtKategoriAd);
+            this.Controls.Add(this.lblKategoriAd);
+            this.Controls.Add(this.txtKategoriID);
+            this.Controls.Add(this.lblKategoriID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kategoriler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKategoriID;
+        private System.Windows.Forms.TextBox txtKategoriID;
+        private System.Windows.Forms.TextBox txtKategoriAd;
+        private System.Windows.Forms.Label lblKategoriAd;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ UrunYonetimWEntityFr/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace UrunYonetimWEntityFr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DBUrunYonetimEntities db = new DBUrunYonetimEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var kategoriler = db.TBL_KATEGORI.ToList();
+            dataGridView1.DataSource = kategoriler;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            TBL_KATEGORI t = new TBL_KATEGORI();
+        }
+    }
+}
